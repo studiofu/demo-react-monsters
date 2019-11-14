13b1115237fe917c7029b32935a54c380b402e14
Johnson Fu <johnson.fu@gmail.com>
2019-11-14 20:36:19 +0800

    update

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
 
     return (
       <div className='App'>
+        <h1>Monsters Rolodex</h1>
         <SearchBox handleChange={this.handleChange} />
         <CardList monsters={filterMonsters} />
       </div>
