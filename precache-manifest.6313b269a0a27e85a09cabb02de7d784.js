024afd51aaa4f470070b1176e19f6916ddb24e90
Johnson Fu <johnson.fu@gmail.com>
2019-11-14 20:35:51 +0800

    Updates

---
+++ precache-manifest.6313b269a0a27e85a09cabb02de7d784.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9af7948481fd3a018f087ef0aea9c3",
+    "url": "/demo-react-monsters/index.html"
+  },
+  {
+    "revision": "5412922d3c69dc1b81ff",
+    "url": "/demo-react-monsters/static/css/main.59c5a1fa.chunk.css"
+  },
+  {
+    "revision": "ae5aea0e670801666108",
+    "url": "/demo-react-monsters/static/js/2.b37a223b.chunk.js"
+  },
+  {
+    "revision": "5412922d3c69dc1b81ff",
+    "url": "/demo-react-monsters/static/js/main.770d04dd.chunk.js"
+  },
+  {
+    "revision": "db50165797e26e0a8d4c",
+    "url": "/demo-react-monsters/static/js/runtime-main.b86d2bb6.js"
+  }
+]);
